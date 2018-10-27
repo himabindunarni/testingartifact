317161fba5d521459827f7bd1f06526bba84b6c0
Taraka <sri4g.j@gmail.com>
2018-10-27 10:47:42 -0500

    first commit

---
+++ src/main/java/testingPackage/TestingPractice.java
@@ -0,0 +1,10 @@
+package testingPackage;
+
+public class TestingPractice {
+
+	public static void main(String[] args) {
+		System.out.println("This is code we want to push to git .....");
+
+	}
+
+}
